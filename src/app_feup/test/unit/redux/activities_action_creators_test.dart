df029fbcd815b36a93f594cc9c46bf123f8427bf
Filipe Campos <filipepcampos24@gmail.com>
2022-06-06 12:12:10 +0100

    Add a couple of activity filters tests

--- src/app_feup/test/unit/redux/activities_action_creators_test.dart
+++ src/app_feup/test/unit/redux/activities_action_creators_test.dart
@@ -78,4 +78,78 @@ void main() {
       expect(actions[1].status, RequestStatus.failed);
     });
   });
+
+  group('Filters Action Creator', () {
+    final fetcherMock = MockActivitiesFetcher();
+    final Tuple2<String, String> userPersistentInfo = Tuple2('', '');
+    final mockStore = MockStore();
+
+    final profile = Profile();
+    profile.courses = [Course(id: 7474)];
+
+    test('By default all filters are active', () async {
+      final content = {
+        'session': Session(authenticated: true),
+        'profile': profile,
+        'currUcs': [
+          CourseUnit(name:'Course 1', abbreviation: 'C1'),
+          CourseUnit(name:'Course 2', abbreviation: 'C2'),
+          CourseUnit(name:'Course 3', abbreviation: 'C3'),
+        ],
+        'filteredActivities': <String, bool>{}
+      };
+      when(mockStore.state).thenReturn(AppState(content));
+
+      final Completer<Null> completer = Completer();
+      final actionCreator =
+          getUserActivities(completer, userPersistentInfo, fetcher: fetcherMock);
+      when(fetcherMock.getActivities(any))
+          .thenAnswer((_) async => []);
+
+      actionCreator(mockStore);
+      await completer.future;
+      final List<dynamic> actions =
+          verify(mockStore.dispatch(captureAny)).captured;
+      expect(actions.length, 4);
+      expect(actions[0].status, RequestStatus.busy);
+      expect(actions[1].filteredActivities, {
+        'Course 1': true, 'Course 2': true, 'Course 3': true
+        });
+      expect(actions[2].activities, []);
+      expect(actions[3].status, RequestStatus.successful);
+    });
+
+    test('If theres any filteredActivities the remaining filters are turned off', () async {
+      final content = {
+        'session': Session(authenticated: true),
+        'profile': profile,
+        'currUcs': [
+          CourseUnit(name:'Course 1', abbreviation: 'C1'),
+          CourseUnit(name:'Course 2', abbreviation: 'C2'),
+          CourseUnit(name:'Course 3', abbreviation: 'C3'),
+        ],
+        'filteredActivities': <String, bool>{'Course 1': true}
+      };
+      when(mockStore.state).thenReturn(AppState(content));
+
+      final Completer<Null> completer = Completer();
+      final actionCreator =
+          getUserActivities(completer, userPersistentInfo, fetcher: fetcherMock);
+      when(fetcherMock.getActivities(any))
+          .thenAnswer((_) async => []);
+
+      actionCreator(mockStore);
+      await completer.future;
+      final List<dynamic> actions =
+          verify(mockStore.dispatch(captureAny)).captured;
+      expect(actions.length, 4);
+      expect(actions[0].status, RequestStatus.busy);
+      expect(actions[1].filteredActivities, {
+        'Course 1': true, 'Course 2': false, 'Course 3': false
+        });
+      expect(actions[2].activities, []);
+      expect(actions[3].status, RequestStatus.successful);
+    });
+
+  });
 }
