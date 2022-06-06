534b6b63daf34a437dae35bf8bb634601f2d4678
Filipe Campos <filipepcampos24@gmail.com>
2022-06-06 17:14:00 +0100

    Add export functionality

--- src/app_feup/lib/view/Widgets/activity_primary_row.dart
+++ src/app_feup/lib/view/Widgets/activity_primary_row.dart
@@ -1,3 +1,5 @@
+import 'package:add_2_calendar/add_2_calendar.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:uni/model/entities/activity.dart';
 import 'package:flutter/material.dart';
 import 'package:uni/view/Widgets/date_rectangle.dart';
@@ -16,17 +18,51 @@ class PrimaryActivityRow extends StatelessWidget {
       DateRectangle(date: this._activity.getWeekDayMonth()),
       GenericRow(
           topBottomPaddingDelta: 10,
-          left: Text(this._activity.getHourMinute(),
-              style: Theme.of(context).textTheme.headline4),
+          left: 
+          Container(
+              margin: EdgeInsets.only(top: 4.0, bottom: 12.0),
+              child: Stack(children: [
+                IconButton(
+                  padding: EdgeInsets.only(top: 5.0, bottom: 12.0),
+                  alignment: Alignment.topLeft,
+                  icon: Icon(MdiIcons.calendarPlus, size: 30),
+                  onPressed: () {
+                    Add2Calendar.addEvent2Cal(this.createActivityEvent());
+                  },
+                ),
+                Container(
+                    margin: EdgeInsets.only(top: 60.0, bottom: 45.0),
+                    child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                          this._activity.getHourMinute(),
+                          style: Theme.of(context).textTheme.headline4)
+                        ])
+                ),
+              ])
+          ),
           center: Column(children: [
             Text(this._activity.getCourseUnit(),
                 style: Theme.of(context)
                     .textTheme
                     .headline3
-                    .apply(fontSizeDelta: 10)),
+                    .apply(fontSizeDelta: 10)
+            ),
             Text(this._activity.getDescription(),
                 style: Theme.of(context).textTheme.headline4)
-          ]))
+          ])
+        )
     ]);
+  }
+
+  createActivityEvent() {
+    return Event(
+      title: _activity.getCourseUnit() + ' - ' + _activity.getDescription(),
+      startDate: _activity.getStart().toLocal(),
+      endDate: _activity.getEnd().toLocal(),
+    );
   }
 }
